f61e5322930c182f3f4805a6022626de4c3ea665
endy90 <56228479+endy90@users.noreply.github.com>
2023-11-30 11:58:43 +0100

    Delete 01_hello_terraform directory

--- 01_hello_terraform/main.tf
+++
@@ -1,9 +0,0 @@
-#TODO Publier une petite instance AWS EC2
-
-provider "aws" {
-  region = "us-east-1"
-}
-resource "aws_instance" "example" {
-  ami           = "ami-0230bd60aa48260c6"
-  instance_type = "t2.micro"
-}
